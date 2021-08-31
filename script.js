e2056cc036b8cc5a91adf34f8367d1a000285fe6
Bharath-Krishna-AB <bharatha.b2007@gmail.com>
2021-08-31 10:02:00 -0700

    score board updating completed

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
-
-
-
 var threeHands = {paper: "/assets/paper.png",scissors: "/assets/scissors.png",rock: "/assets/rock.png"}
+var score= 0
+
 
 function pickUserHand(userInp){
 console.log(userInp);
 
+//hiding hands page
+
 var hideHands =document.querySelector('.hands')
 hideHands.style.display = 'none'
+
+//showing contest page
 
 var showResult = document.querySelector('.contest')
 showResult.style.display = 'flex'
 
-console.log(threeHands.paper);
+//setting img src according to user picks
 
 var userhand =document.getElementById('userPickImage')
 if(userInp == 'paper'){
@@ -29,14 +32,18 @@ if(userInp == 'paper'){
     
 }
 
+//picked computer choice randomly
+
 let threeOpts = ['paper','scissors','rock']
 let cpRandom= threeOpts[Math.floor(Math.random() * threeOpts.length)]
 
 
 
+//setting img src according to computer picks
+
 var computerhand = document.getElementById('computerPickImage')
 
-if(cpRandom == 'paper'){
+ if(cpRandom == 'paper'){
 
     computerhand.src = `${threeHands.paper}`    
 
@@ -50,6 +57,50 @@ if(cpRandom == 'paper'){
     
 }
 
+//setting user Wins or tie in h1 tag
+
+var decision =document.querySelector('.decision h1')
+
+if(userInp == cpRandom){
+    decision.innerText = "ITS A TIE!"
+}else if(userInp=='paper' && cpRandom=='rock') {
+    decision.innerText = "YOU WIN!"
+    score++
+}else if(userInp=='rock' && cpRandom=='paper') {
+    decision.innerText = "COMPUTER WIN!"
+}else if(userInp=='scissors' && cpRandom=='paper') {
+    decision.innerText = "YOU WIN!"
+    score++
+}else if(userInp=='paper' && cpRandom=='scissors') {
+    decision.innerText = "COMPUTER WIN!"
+}else if(userInp=='scissors' && cpRandom=='rock') {
+    decision.innerText = "COMPUTER WIN!"
+}else if(userInp=='rock' && cpRandom=='scissors') {
+    decision.innerText = "YOU WIN!"
+    score++
+}
+
+//updating score
+
+var updatingScore=document.querySelector('.score h1')
+
+updatingScore.innerText=`${score}`
 
 
 }
+
+function restartGame(){
+    
+//showing hands page
+
+var hideHands =document.querySelector('.hands')
+hideHands.style.display = 'flex'
+
+//hiding contest page
+
+var showResult = document.querySelector('.contest')
+showResult.style.display = 'none'
+
+console.log(score);
+}
+
