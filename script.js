b24bafb9b05ddb270ceb368459df03113e30929c
Bharath-Krishna-AB <bharatha.b2007@gmail.com>
2021-08-31 09:06:31 -0700

    setted auto pick from server side

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
-var hideHands =document.querySelector('.hands')
 
-var showResult = document.querySelector('.contest')
 
-var userhand =document.getElementById('userPickImage')
 
 var threeHands = {paper: "/assets/paper.png",scissors: "/assets/scissors.png",rock: "/assets/rock.png"}
 
 function pickUserHand(userInp){
 console.log(userInp);
+
+var hideHands =document.querySelector('.hands')
 hideHands.style.display = 'none'
 
+var showResult = document.querySelector('.contest')
 showResult.style.display = 'flex'
 
 console.log(threeHands.paper);
+
+var userhand =document.getElementById('userPickImage')
 if(userInp == 'paper'){
 
     userhand.src = `${threeHands.paper}`    
@@ -26,5 +28,28 @@ if(userInp == 'paper'){
     userhand.src = `${threeHands.rock}`    
     
 }
+
+let threeOpts = ['paper','scissors','rock']
+let cpRandom= threeOpts[Math.floor(Math.random() * threeOpts.length)]
+
+
+
+var computerhand = document.getElementById('computerPickImage')
+
+if(cpRandom == 'paper'){
+
+    computerhand.src = `${threeHands.paper}`    
+
+}else if(cpRandom == 'scissors'){
+
+    computerhand.src = `${threeHands.scissors}`    
+    
+}else if(cpRandom == 'rock'){
+
+    computerhand.src = `${threeHands.rock}`    
+    
+}
+
+
 
 }
