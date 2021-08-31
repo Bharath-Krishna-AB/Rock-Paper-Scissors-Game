548410272ef94c15c7635b2c94b8b4c9821b5ae2
Bharath-Krishna-AB <bharatha.b2007@gmail.com>
2021-08-31 08:08:55 -0700

    design completed of result section

---
+++ script.js
@@ -0,0 +1,10 @@
+var hideHands =document.querySelector('.hands')
+
+var threeHands = ["paper","scissors","rock"]
+
+function pickUserHand(userInp){
+console.log(userInp);
+hideHands.style.visibility = 'hidden'
+
+
+}
