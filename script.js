7ff6599a64a0c7a7792a3699f53c0a5a72bbe65d
Bharath-Krishna-AB <bharatha.b2007@gmail.com>
2021-08-31 08:51:26 -0700

    user hand picking img and giving it to the next page comppleted

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
 var hideHands =document.querySelector('.hands')
 
-var threeHands = ["paper","scissors","rock"]
+var showResult = document.querySelector('.contest')
+
+var userhand =document.getElementById('userPickImage')
+
+var threeHands = {paper: "/assets/paper.png",scissors: "/assets/scissors.png",rock: "/assets/rock.png"}
 
 function pickUserHand(userInp){
 console.log(userInp);
-hideHands.style.visibility = 'hidden'
+hideHands.style.display = 'none'
 
+showResult.style.display = 'flex'
+
+console.log(threeHands.paper);
+if(userInp == 'paper'){
+
+    userhand.src = `${threeHands.paper}`    
+
+}else if(userInp == 'scissors'){
+
+    userhand.src = `${threeHands.scissors}`    
+    
+}else if(userInp == 'rock'){
+
+    userhand.src = `${threeHands.rock}`    
+    
+}
 
 }
